5b2cd040604f64438ea5dec304b38f695f34ab1f
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-09-28 21:39:34 +0600

    Updated App file

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,48 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { useRecoilState } from 'recoil';
-import { useSocket } from '../hooks/useSocket';
-import { presentationState } from '../hooks/usePresentation';
-import SlideArea from './SlideArea';
-import { Container, Grid } from '@mui/material';
+import { useState } from 'react';
+import Homepage from "../Home/Homepage";
+import NicknameInput from "./NicknameInput";
 
 const App = () => {
   const [nickname, setNickname] = useState('');
-  const [connected, setConnected] = useState(false);
-  const [presentation, setPresentation] = useRecoilState(presentationState);
-  const socket = useSocket(presentation.id);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    if (connected) {
-      socket.on('presentationData', (data) => {
-        setPresentation(data);
-      });
+  const handleLogin = () => {
+    if (nickname.trim()) {
+      setIsLoggedIn(true);
     }
-  }, [connected, socket, setPresentation]);
-
-  const handleJoin = () => {
-    socket.emit('join', { nickname });
-    setConnected(true);
   };
 
-  return !connected ? (
-    <div className="nickname-input">
-      <input
-        type="text"
-        value={nickname}
-        onChange={(e) => setNickname(e.target.value)}
-        placeholder="Enter your nickname"
-      />
-      <button onClick={handleJoin}>Join</button>
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      {!isLoggedIn ? (
+        <NicknameInput setNickname={setNickname} handleLogin={handleLogin} />
+      ) : (
+        <div className="w-full h-full">
+          <Homepage nickname={nickname} />
+        </div>
+      )}
     </div>
-  ) : (
-    <Container>
-      <Grid container spacing={2}>
-        {/* grid items here */}
-        <Grid item xs={10}>
-          <SlideArea />
-        </Grid>
-      </Grid>
-    </Container>
   );
 };
 
