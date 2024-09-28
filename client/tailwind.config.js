5a78d4ba5d3a5277e6269dfe5a3a4d0d8ec72879
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-09-29 00:38:29 +0600

    Updated the packages, tailwind config

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust this line based on the file extensions you use
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
